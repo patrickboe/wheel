1b1e8025d68267f934bfda622d7bf05b6ccb306d
Patrick Boe <patrick@hametz>
2016-04-07 06:09:50 -0400

    minor simplifications

--- src/main/cljs/wheel/auth.cljs
+++ src/main/cljs/wheel/auth.cljs
@@ -8,16 +8,17 @@
     "aC17miYcAZM4eBV4N1slaTaPiCazJN6o"
     "patrickboe.auth0.com"))
 
+(defn ^:private read-token []
+  (js->clj (.parseHash lock (aget js/window "location" "hash"))))
+
 (defn ^:private read-user []
-  (or
-    (js->clj (.parseHash lock (aget (aget js/window "location") "hash")))
-    :anonymous))
+  (or (read-token) :anonymous))
 
 (defn ^:private login-dialog []
   (.show lock #js {:authParams #js {:scope "openid"}}))
 
-(defn remote [{:keys [auth]} cb]
-  (match auth
-         [:user] (cb {:user (read-user)})
-         [(['user/login] :seq)] (login-dialog)
+(defn remote [msg cb]
+  (match msg
+         {:auth [:user]} (cb {:user (read-user)})
+         {:auth [(['user/login] :seq)]} (login-dialog)
          _ nil))
